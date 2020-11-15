a3b73b43cb0fa4486a522103aa0a9ba33da483e7
Kayla Newlon <kbnewlon@hotmail.com>
2020-11-14 22:12:17 -0800

    finalising portfolio styling and add readme

--- README.md
+++ README.md
@@ -25,13 +25,16 @@ This app is designed to provide future possible employers a way to see my work, 
 4. Enjoy!
 
 ## Usage
-1. 
+1. Click on the heroku link below to view my portfolio, made in React
+2. This application is split up into three pages and four components
+3. The cover page uses the navbar, header, and footer components
+4. The about me page uses the navbar and footer components
+5. The portfolio page uses the navbar, project, and footer components
 
 ## Technologies
 * HTML
 * CSS 
 * JavaScript
-* Axios 
 * React.js
 * Bootstrap 
 
@@ -45,141 +48,17 @@ N/A
 N/A
 
 ## Visuals
-![]()
+![screenshot of react portfolio homepage](assets/screenshot_homepage_react_portfolio.PNG)
+![screenshot of about me page](assets/screenshot_about_me.PNG)
+![screenshot of portfolio page](assets/screenshot_portfolio.PNG)
 
 
 ## Links
 To Repo: https://github.com/kbnewlon/portfolio-react
-<br>Visit the deployed app on Heroku to demo: 
+<br>Visit the deployed app on Heroku to demo: https://kbnewlon-react-portfolio.herokuapp.com/
  
 
 ## Questions 
 Please contact me if you have any questions at:
 <br>Email: kayla.b.newlon@gmail.com
 <br>Github: https://github.com/kbnewlon
-
-
-
-more project code 
- <div className="element-item design development marketing seo">
-                    <a className="popup-with-move-anim" href="#project-5">
-                      <div className="element-item-overlay"><span>Joy Moments</span></div><img
-                        src="images/project-5.jpg" alt="alternative" />
-                    </a>
-                  </div>
-                  <div className="element-item design marketing seo">
-                    <a className="popup-with-move-anim" href="#project-6">
-                      <div className="element-item-overlay"><span>Spark Events</span></div><img
-                        src="images/project-6.jpg" alt="alternative" />
-                    </a>
-                  </div>
-                  <div className="element-item design marketing">
-                    <a className="popup-with-move-anim" href="#project-7">
-                      <div className="element-item-overlay"><span>Casual Wear</span></div><img
-                        src="images/project-7.jpg" alt="alternative" />
-                    </a>
-                  </div>
-                  <div className="element-item design marketing">
-                    <a className="popup-with-move-anim" href="#project-8">
-                      <div className="element-item-overlay"><span>Zazoo Apps</span></div><img
-                        src="images/project-8.jpg" alt="alternative" />
-                    </a>
-                  </div>
-                </div>
-# Unit 20 React Homework: React Portfolio
-
-Now that you've worked with React and have multiple projects to share, you'll be updating your portfolio and other materials to build toward being employer competitive. Creating a portfolio using React will help set you apart from other developers whose portfolios do not use some of the latest technologies.
-
-If you are opting out of career services, this is *still a required assignment*. Part of being a web developer means being a part of a community. Having a place to share your projects is *necessary* if you're applying for jobs, but is still *critical* on your journey as a developer.
-
-## Requirements
-
-* Updated portfolio featuring 6 total projects
-
-* Use React
-
-* A `Header` component that appears on multiple pages
-
-* A single `Project` component that will be used multiple times on a single page 
-
-* Navigation with React Router, dynamic rendering, or another third part router
-
-* A `Footer` component that appears on multiple pages
-
-* Update GitHub profile with pinned repositories featuring those same projects
-
-## Grading
-
-| Requirement    | Weight |
-| -------------- | ------ |
-| Portfolio      | 90%    |
-| GitHub Profile | 10%    |
-
-
-## Instructions
-
-* [Updated Portfolio](#updated-portfolio)
-
-* [Design](#design)
-
-* [React](#react)
-
-* [Updated LinkedIn Profile](#updated-linkedin-profile)
-
-### Updated Portfolio
-
-Your updated site should still have all of the content it previously had:
-and y
-* Your name
-
-* Links to your GitHub profile & LinkedIn page as well as your email address and phone number
-
-* A link to a PDF of your resume with updated projects
-
-* A list of projects. For each project, make sure you have the following:
-
-  * Project title
-
-  * Link to the deployed version
-
-  * Link to the GitHub repository
-
-  * GIF or screenshot of the deployed application
-
-
-#### Design
-
-As with the previous portfolio homework, "good" design is subjective. Your site should look
-"polished." Here are a few guidelines on what that means:
-
-* Mobile-first design
-
-* Choose a color palette for your site so it doesn't just look like
-the default bootstrap theme or an unstyled HTML site. You may we
-
-* Make sure the font size is large enough to read, and that the colors don't cause eye strain.
-
-* If you want to go above and beyond, try using animations and react component libraries. Note 
-that this will _not_ affect your grade, but it may impact how potentials employers gauge your knowledge.
-
-### React
-
-Additionally, this new portfolio should be created using React.
-
-At a minimum, your portfolio should include the following:
-
-* A `Header` component that appears on multiple pages
-
-* A single `Project` component that will be used multiple times on a single page 
-
-* Navigation with React Router, dynamic rendering, or another third part router
-
-* A `Footer` component that appears on multiple pages
-
-
-### Updated LinkedIn Profile 
-
-Make sure to update your LinkedIn Profile with the new skills you've acquired since the last time it was updated.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
---
+++ src/component/Projects/index.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './style.css';
+
+
+function Projects() {
+    return (
+        <>
+            <h1 className="headerPortfolio">PROJECTS IM PROUD OF</h1>
+            <Container className="portfolioDiv">
+                <Row>
+                    <Col md={4}>
+                        <Card className="portfolioCard" style={{ width: '24rem' }} >
+                            <Card.Img variant="top" src="images/screenshot_homepage.PNG" />
+                            <Card.Body>
+                                <Card.Title>The Cold West Tour Co</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">MySQL2, Express, Handlebars, Sequelize</Card.Subtitle>
+                                <Card.Text>
+                                    <p>Join The Cold West Tours this winter and find your perfect outdoor activity for every mood, style, and condition. Your adventure awaits.</p>
+                                    <p>Our team has together all ten ski and snowboard resorts in Washington State. Start your winter sport planning here! Create an account with us to save your favorite winter activity and favorite lodge. We hope you like it!</p>
+                                    <p>*not a real tour company</p>
+                                </Card.Text>
+                                <Card.Link href="https://github.com/kbnewlon/burger">GO TO GITHUB REPO</Card.Link>
+                                <Card.Link href="https://a-virtual-burger-shack.herokuapp.com/">GO TO DEPLOYED APP</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col md={4}>
+                        <Card className="portfolioCard" style={{ width: '24rem' }}>
+                            <Card.Img variant="top" src="images/screenshot_covid_tracker.png" />
+                            <Card.Body>
+                                <Card.Title>Covid-19 Resource Page</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">APIs, Pure.CSS, Interactive</Card.Subtitle>
+                                <Card.Text>
+                                    <p> Use this app as a hub of peer reviewed, reliable, Covid information. This application was developed to monitor the density of COVID-19 cases by county as well as to give the user resources for the pandemic. Along with an interactive map of active cases, we have supplied a COVID-19 testing location finder, recent top peer reviewed articles related to the virus as well as a link to the Centers for Disease Control and Prevention (CDC) homepage.</p>
+                                </Card.Text>
+                                <Card.Link href="https://github.com/jjainga/Covid-19-Resource-Page">GO TO GITHUB REPO</Card.Link>
+                                <Card.Link href="https://jjainga.github.io/Covid-19-Resource-Page/">GO TO DEPLOYED APP</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col md={4}>
+                        <Card className="portfolioCard" style={{ width: '24rem' }}>
+                            <Card.Img variant="top" src="images/screenshot_burger_app.PNG" />
+                            <Card.Body>
+                                <Card.Title>It's Come To This:<br /> A Virtual Burger Shack</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">MySQL, Node, Express, Handlebars, ORM</Card.Subtitle>
+                                <Card.Text>
+                                    <p>Quarantine keeping you from eating what you really want? This virtual burger application combines the use MySQL, Node, Express, Handlebars, as well as its very own homemade ORM (yum!)</p>
+                                    <p> Don't bother with those pesky germs, Use this app to create and DEVOUR your very own virtual burger.</p>
+                                    <p>Bon appetit!</p>
+                                </Card.Text>
+                                <Card.Link href="https://github.com/kbnewlon/Cold-West-Tours">GO TO GITHUB REPO</Card.Link>
+                                <Card.Link href="https://the-cold-west.herokuapp.com/">GO TO DEPLOYED APP</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card className="portfolioCard" style={{ width: '24rem' }}>
+                            <Card.Img variant="top" src="images/screenshot_employee_tracker.PNG" />
+                            <Card.Body>
+                                <Card.Title>Content Management System</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Node, Inquirer, MySQL</Card.Subtitle>
+                                <Card.Text>
+                                    <p>An easy to use interface for non-developers to view and interact with information stored in databases.
+                                    This command-line application allows users to:
+                                    <br /><br />
+                                           <ul>
+                                            <li>Add departments, roles, employees</li>
+                                            <li>View departments, roles, employees</li>
+                                            <li>Update employee roles</li>
+                                        </ul>
+                                    </p>
+                                </Card.Text>
+                                <Card.Link href="https://github.com/kbnewlon/employee-tracker">GO TO GITHUB REPO</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                    <Col md={4}>
+                        <Card className="portfolioCard" style={{ width: '24rem' }}>
+                            <Card.Img variant="top" src="images/screenshot_employee_directory.PNG" />
+                            <Card.Body>
+                                <Card.Title>Employee Directory</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">React, API</Card.Subtitle>
+                                <Card.Text>
+                                    <p>This application has been created with an random user API to compile a faux employee directory using React. The application has been split up into three components to display the image, name, phone number, email address, and age of each 'employee'.</p>
+                                </Card.Text>
+                                <Card.Link href="https://github.com/kbnewlon/employee-directory">GO TO GITHUB REPO</Card.Link>
+                                <Card.Link href="https://kbn-employee-directory.herokuapp.com/">GO TO DEPLOYED APP</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                    <Col md={4}>
+                        <Card className="portfolioCard" style={{ width: '24rem' }}>
+                            <Card.Img variant="top" src="images/screenshot_workout-tracker.PNG" />
+                            <Card.Body>
+                                <Card.Title>Fitness Tracker 2020</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Express, Mongo DB, Mongoose</Card.Subtitle>
+                                <Card.Text>
+                                    <p> Use this application to view, create and track daily workouts. The user can log multiple exercises in a workout on a given day. Fitness Tracker 2020 allows the user to be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, the user can track the distance traveled.</p>
+                                </Card.Text>
+                                <Card.Link href="https://github.com/kbnewlon/workout-trackere">GO TO GITHUB REPO</Card.Link>
+                                <Card.Link href="https://preparingfortheapocalypse2020.herokuapp.com/">GO TO DEPLOYED APP</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default Projects;
